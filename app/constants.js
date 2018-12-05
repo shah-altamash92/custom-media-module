56f8706f6b3e8efdbe5c513dab0e1dd69667d901
shah-altamash92 <43642211+shah-altamash92@users.noreply.github.com>
2018-12-05 16:09:26 +0530

    Update constants.js

--- app/constants.js
+++ app/constants.js
@@ -1,5 +1,5 @@
 
-export const IOS_EDITOR_KEY = '356ccf6a64237931c06a32f16f37dd1494227e57'
+export const IOS_EDITOR_KEY = 'bec6752f27bdf30cdd25c40cdd63675d201be9c6'
 export const ANDROID_EDITOR_KEY = 'b3592e51d3bd33650f8774260972e31a607bbdc0'
 
 export const MAX_MEDIA_UPLOAD = 10;
